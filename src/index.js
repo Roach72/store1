85637ac2380a8c49f60acd8492a7dee0016e2229
Roach72 <103392055+Roach72@users.noreply.github.com>
2022-04-11 16:40:43 +0200

    store
    store

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/index.css';
+import 'bootstrap';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin= require("mini-css-extract-plugin")
+var OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry:  {
+    app:'./src/index.js'
+  },
+  
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "main.js"
+  },
+  
+  mode: "development",
+
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 9000,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    hot: false, //تساعد في التحديث التلقائي
+    liveReload: true,//تساعد في التحديث التلقائي
+    open: true,// لفتح المشروع مباشرة بعد تنفيذ الأمر
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+      
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        ],
+      },
+    ]
+
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    
+    new MiniCssExtractPlugin({ filename: "css/index.css"}),
+
+    new OptimizeCSSAssetsPlugin({}),
+
+  ],
+
+};
